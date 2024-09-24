9c9f886838b04aaec1367a08613378e73939375a
D.Yogesh <yogeshone678@gmail.com>
2024-09-25 02:03:18 +0530

    fix: add test route
    Signed-off-by: D.Yogesh <yogeshone678@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./src/database/db");
 const authRoutes = require("./src/routes/auth");
 const profileRoutes = require("./src/routes/profile");
 const todoRoutes = require("./src/routes/todo");
@@ -21,6 +20,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/todos", todoRoutes);
 
+app.get("/", (req, res) => {
+  res.send("#### test!");
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on :${port}`);
 });
